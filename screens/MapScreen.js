f20053ae98da6bb7aec0072946b9f983acae0953
BrianDeairng <>
2019-11-04 17:57:00 -0600

    No Heatmap, Only clusters
    No longer using heatmap.
    We will be using clusters of points instead.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -6,9 +6,10 @@ import {
   Text,
   ScrollView
 } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
+import { Marker } from 'react-native-maps';
 
-export default class MapScreen extends Component<Props> {
+export default class MapScreen extends Component {
   
   
   onRegionChange(region) {
@@ -17,25 +18,48 @@ export default class MapScreen extends Component<Props> {
 
 
   render() {
-      return (
-        // <MapView
-        //   region={{
-        //     latitude: 37.78825,
-        //     longitude: -122.4324,
-        //     latitudeDelta: 0.0922,
-        //     longitudeDelta: 0.0421,
-        //   }}
-        //   onRegionChange={this.onRegionChange}
-        // />
-        <MapView style={{flex: 1}}
-        region={{latitude: 36.0694066,
-                longitude: -94.1748312,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421
-              }}
+    
+    return(
+      <MapView  
+        region={{  
+          latitude: 52.5,  
+          longitude: 19.2,  
+          latitudeDelta: 8.5,  
+          longitudeDelta: 8.5  
+        }}  
+        style={{ width: 400, height: 800 }}  
         showsUserLocation={true}
-        />
-      );
+        provider={"google"}
+      >  
+        <Marker coordinate={{ latitude: 52.0, longitude: 18.2 }} />  
+        <Marker coordinate={{ latitude: 52.4, longitude: 18.7 }} />  
+        <Marker coordinate={{ latitude: 52.1, longitude: 18.4 }} />  
+        <Marker coordinate={{ latitude: 52.6, longitude: 18.3 }} />  
+        <Marker coordinate={{ latitude: 51.6, longitude: 18.0 }} />  
+        <Marker coordinate={{ latitude: 53.1, longitude: 18.8 }} />  
+        <Marker coordinate={{ latitude: 52.9, longitude: 19.4 }} />  
+        <Marker coordinate={{ latitude: 52.2, longitude: 21 }} />  
+      </MapView>
+    );
+      // return (
+      //   // <MapView
+      //   //   region={{
+      //   //     latitude: 37.78825,
+      //   //     longitude: -122.4324,
+      //   //     latitudeDelta: 0.0922,
+      //   //     longitudeDelta: 0.0421,
+      //   //   }}
+      //   //   onRegionChange={this.onRegionChange}
+      //   // />
+      //   <MapView style={{flex: 1}}
+      //   region={{latitude: 36.0694066,
+      //           longitude: -94.1748312,
+      //           latitudeDelta: 0.0922,
+      //           longitudeDelta: 0.0421
+      //         }}
+      //   showsUserLocation={true}
+      //   />
+      // );
     }
 }
 
