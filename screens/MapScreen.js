28b7d02067faffd3c947c76a08531857d8cfef3b
ColdMold <44824348+ColdMold@users.noreply.github.com>
2019-11-20 18:34:21 -0600

    Update MapScreen.js
    am fetching json from server, still dont know how to access the latitude and longitude from this to put into markets

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,76 +1,101 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   View,
   Text,
-  ScrollView
-} from 'react-native';
-import MapView from 'react-native-map-clustering';
-import { Marker } from 'react-native-maps';
+  ScrollView,
+  ActivityIndicator
+} from "react-native";
+import MapView from "react-native-map-clustering";
+import { Marker } from "react-native-maps";
 
 export default class MapScreen extends Component {
-  
-  
   onRegionChange(region) {
     this.setState({ region });
   }
 
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount() {
+    return fetch("http://maincomputer.myvnc.com:8081/points/")
+      .then(response => response.json())
+      .then(responseJson => {
+        alert(responseJson); //test to show I am fetching the right information, still don't know how to get lat, long from responseJson to put into markers
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   render() {
-    
-    return(
-      <MapView  
-        region={{  
-          latitude: 52.5,  
-          longitude: 19.2,  
-          latitudeDelta: 8.5,  
-          longitudeDelta: 8.5  
-        }}  
-        style={{ width: 400, height: 800 }}  
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <MapView
+        region={{
+          latitude: 52.5,
+          longitude: 19.2,
+          latitudeDelta: 8.5,
+          longitudeDelta: 8.5
+        }}
+        style={{ width: 400, height: 800 }}
         showsUserLocation={true}
         provider={"google"}
-      >  
-        <Marker coordinate={{ latitude: 52.0, longitude: 18.2 }} />  
-        <Marker coordinate={{ latitude: 52.4, longitude: 18.7 }} />  
-        <Marker coordinate={{ latitude: 52.1, longitude: 18.4 }} />  
-        <Marker coordinate={{ latitude: 52.6, longitude: 18.3 }} />  
-        <Marker coordinate={{ latitude: 51.6, longitude: 18.0 }} />  
-        <Marker coordinate={{ latitude: 53.1, longitude: 18.8 }} />  
-        <Marker coordinate={{ latitude: 52.9, longitude: 19.4 }} />  
-        <Marker coordinate={{ latitude: 52.2, longitude: 21 }} />  
+      >
+        <Marker coordinate={{ latitude: 52.0, longitude: 18.2 }} />
+        <Marker coordinate={{ latitude: 52.4, longitude: 18.7 }} />
+        <Marker coordinate={{ latitude: 52.1, longitude: 18.4 }} />
+        <Marker coordinate={{ latitude: 52.6, longitude: 18.3 }} />
+        <Marker coordinate={{ latitude: 51.6, longitude: 18.0 }} />
+        <Marker coordinate={{ latitude: 53.1, longitude: 18.8 }} />
+        <Marker coordinate={{ latitude: 52.9, longitude: 19.4 }} />
+        <Marker coordinate={{ latitude: 52.2, longitude: 21 }} />
       </MapView>
     );
-      // return (
-      //   // <MapView
-      //   //   region={{
-      //   //     latitude: 37.78825,
-      //   //     longitude: -122.4324,
-      //   //     latitudeDelta: 0.0922,
-      //   //     longitudeDelta: 0.0421,
-      //   //   }}
-      //   //   onRegionChange={this.onRegionChange}
-      //   // />
-      //   <MapView style={{flex: 1}}
-      //   region={{latitude: 36.0694066,
-      //           longitude: -94.1748312,
-      //           latitudeDelta: 0.0922,
-      //           longitudeDelta: 0.0421
-      //         }}
-      //   showsUserLocation={true}
-      //   />
-      // );
-    }
+    // return (
+    //   // <MapView
+    //   //   region={{
+    //   //     latitude: 37.78825,
+    //   //     longitude: -122.4324,
+    //   //     latitudeDelta: 0.0922,
+    //   //     longitudeDelta: 0.0421,
+    //   //   }}
+    //   //   onRegionChange={this.onRegionChange}
+    //   // />
+    //   <MapView style={{flex: 1}}
+    //   region={{latitude: 36.0694066,
+    //           longitude: -94.1748312,
+    //           latitudeDelta: 0.0922,
+    //           longitudeDelta: 0.0421
+    //         }}
+    //   showsUserLocation={true}
+    //   />
+    // );
+  }
 }
 
 MapScreen.navigationOptions = {
-  title: 'Maps',
+  title: "Maps"
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
