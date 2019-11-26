86fcf51e776cd52e29cf88afd02fb64d17570301
ColdMold <44824348+ColdMold@users.noreply.github.com>
2019-11-25 19:23:32 -0600

    Update form2.js
    working on posting points to server

--- screens/form2.js
+++ screens/form2.js
@@ -5,27 +5,25 @@ import {
   View,
   Text,
   ScrollView,
-  ActivityIndicator,
+  ActivityIndicator
+} from "react-native";
 
-} from 'react-native';
+import { Button } from "react-native-elements";
 
-import { Button } from 'react-native-elements';
-
-import t from 'tcomb-form-native';
+import t from "tcomb-form-native";
 
 const Form = t.form.Form;
 
 var options = {
   fields: {
     date: {
-      mode: 'date' // display the Date field as a DatePickerAndroid
+      mode: "date" // display the Date field as a DatePickerAndroid
     },
-    time:{
-      mode: 'time'
+    time: {
+      mode: "time"
     }
   }
 };
-
 
 const pointForm = t.struct({
   latitude: t.Number,
@@ -34,42 +32,50 @@ const pointForm = t.struct({
   time: t.Date
 });
 
-
-
 export default class InportForm extends Component<Props, State> {
-
-  constructor(props: Props){
+  constructor(props: Props) {
     super(props);
     this.state = {
-      modalVisible: true,
-    }
+      modalVisible: true
+    };
   }
 
-  setModalVisible=(visible) => {
-    this.setState({modalVisible: visible});
-  }
-
+  setModalVisible = visible => {
+    this.setState({ modalVisible: visible });
+  };
 
   render() {
-    
-    return(
+    return (
       // <form name="myForm" action="" onsubmit="" method="post">
       // latitude: <input type="text" name="latitude" value="latitude"><br>
       // longitude: <input type="text" name="longitude" value="longitude"><br>
       // date: <input type="date" name="date" value="date"><br>
       // time: <input type="time" name="time" value="time"><br>
       // <input type="submit" value="Submit">
-      // </form>   
+      // </form>
 
-      
-
-      <View style={{paddingTop: 20}}>
+      <View style={{ paddingTop: 20 }}>
         <Form type={pointForm} options={options} />
         <Button
           title="Submit"
           type="solid"
           onPress={() => {
-            setModalVisible(!this.state.modalVisible);
+            fetch("http://maincomputer.myvnc.com:8081/point/", {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              },
+              //lat currently does not work, is posting 0.0 to server
+              //lon is hard coded as I can't type negative numbers in form
+              //time is just set at 1 for testing, will work on converting to epoch
+              //once I can figure out how to actually get data from form
+              body: JSON.stringify({
+                lat: pointForm.latitude,
+                lon: -94.179311,
+                time: 1
+              })
+            });
           }}
         />
       </View>
