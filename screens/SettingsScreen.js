cafc29d098976ac171d1258ababb7be3504685dc
AndyHuh98 <andrewhe@uark.edu>
2019-11-28 11:35:42 -0600

    settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,36 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {
   View,
   Text,
-  Switch
+  Switch,
+  StyleSheet,
 } from 'react-native';
 
 import { ExpoConfigView } from '@expo/samples';
 import { Button } from 'react-native-elements';
+import { MapScreen } from './MapScreen.js'
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
+export default class SettingsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue: false,
+    };
+  }
 
-
-   */
-
+  toggleSwitch = (value) => {
+    this.setState({switchValue: value})
+  }
   
-  
-  
-  return (
-    <View>
-      <Text>Settings Screen</Text>
-      <Switch
-        value={true}
-        onValueChange={() => value = !this.value}
-      />
-    </View>
-  )
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>Settings Screen</Text>
+        <Switch
+          value={this.state.switchValue}
+          onValueChange={this.toggleSwitch}
+        />
+      </View>
+    )
+  }
 }
 
+const styles = StyleSheet.create ({  
+     container: {  
+         flex: 1,  
+         alignItems: 'center',  
+         //justifyContent: 'center',  
+     },  
+    textStyle:{  
+        margin: 24,  
+        fontSize: 25,  
+        fontWeight: 'bold',  
+        textAlign: 'center',  
+        color: '#344953'  
+    }  
+})  
+
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  title: 'Settings',
 };
+
