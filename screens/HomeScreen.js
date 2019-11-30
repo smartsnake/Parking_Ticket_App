7ddfc1f64c53d5b885831575e43d101520090011
ColdMold <44824348+ColdMold@users.noreply.github.com>
2019-11-30 17:19:37 -0600

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,6 +9,18 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <Swiper style={styles.wrapper} loop={false}>
+        <View style={styles.textSlide}>
+          <Text style={styles.text}>
+            Welcome to the UARK Parking Ticket App {"\n"} {"\n"}
+            {"\n"}
+            Use this app to make informed decisions when parking on campus
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            The following images will {"\n"}give a brief overview {"\n"}of how
+            to use this app to its fullest potential
+          </Text>
+        </View>
         <View style={styles.slide}>
           <Image style={styles.image} source={require("./images/1.png")} />
         </View>
@@ -40,10 +52,16 @@ const styles = {
     justifyContent: "center",
     backgroundColor: "transparent"
   },
+  textSlide: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#163086"
+  },
   text: {
     color: "#fff",
     fontSize: 30,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    textAlign: "center"
   },
   image: {
     width,
